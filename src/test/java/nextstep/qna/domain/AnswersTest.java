fe2ccb6a4abdf806d0d141a005378670e838028d
yumble <hnjn1101@gmail.com>
2023-12-01 21:16:37 +0900

    fix: DeleteHistory Fixture 변수 선언

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -14,16 +14,20 @@ public class AnswersTest {
 
     private final Question questionByJavajigi = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     private final Answer answerByJavajigi_1 = new Answer(NsUserTest.JAVAJIGI, questionByJavajigi, "Answers Contents1");
-    private final Answer answerByJavajigi_2 = new Answer(NsUserTest.JAVAJIGI, questionByJavajigi, "Answers Contents1_2");
+    private final Answer answerByJavajigi_2 = new Answer(NsUserTest.JAVAJIGI, questionByJavajigi,
+            "Answers Contents1_2");
     private final Answer answerBySanjigi = new Answer(NsUserTest.SANJIGI, questionByJavajigi, "Answers Contents2");
     private final Answers answersBySameWriter = new Answers(Arrays.asList(answerByJavajigi_1, answerByJavajigi_2));
 
 
     @Test
     public void delete_답변글들_작성자_동일시_삭제목록확인() throws CannotDeleteException {
-        List<DeleteHistory> expected = Arrays.asList(
-                new DeleteHistory(ContentType.ANSWER, answerByJavajigi_1.getId(), answerByJavajigi_1.getWriter(), LocalDateTime.now()),
-                new DeleteHistory(ContentType.ANSWER, answerByJavajigi_2.getId(), answerByJavajigi_2.getWriter(), LocalDateTime.now()));
+        DeleteHistory deleteHistory1ByJavajigiAnswer = new DeleteHistory(ContentType.ANSWER, answerByJavajigi_1.getId(),
+                answerByJavajigi_1.getWriter(), LocalDateTime.now());
+        DeleteHistory deleteHistory2ByJavajigiAnswer = new DeleteHistory(ContentType.ANSWER, answerByJavajigi_2.getId(),
+                answerByJavajigi_2.getWriter(), LocalDateTime.now());
+
+        List<DeleteHistory> expected = Arrays.asList(deleteHistory1ByJavajigiAnswer, deleteHistory2ByJavajigiAnswer);
 
         assertThat(answersBySameWriter.delete(NsUserTest.JAVAJIGI))
                 .isEqualTo(expected);
